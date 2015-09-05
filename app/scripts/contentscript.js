56fdd46e9378b4e2c7048d68d3a264d13d500b09
Kazunori Jo <itochan315@gmail.com>
2015-09-06 01:11:40 +0900

    Show console.log() playing songs

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,43 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+var song = {};
+var timer = setInterval(function() {
+  var songNow = {};
+
+  songNow.title = document.getElementById('player-song-title').innerText;
+  songNow.artist = document.getElementById('player-artist').innerText;
+  songNow.album = document.getElementsByClassName('player-album')[0].innerText;
+
+  if (JSON.stringify(song) != JSON.stringify(songNow)) {
+    song.title = songNow.title;
+    song.artist = songNow.artist;
+    song.album = songNow.album;
+
+    notify(song);
+  }
+}, 1000);
+
+function notify(music) {
+  // alert('🎵  ' + trackToString(track));
+  console.log('🎵  ' + trackToString(music));
+  // postToSlack(messageForSlack(track, music), music, track);
+}
+
+// function messageForSlack(track, music) {
+//   function link(text, url) {
+//     return '<' + url + '|' + text + '>'
+//   }
+//
+//   var trackStr = track.name
+//
+//   var url = music && music.trackViewUrl
+//   var message = url ? link(trackStr, url) : trackStr
+//
+//   return '🎵  ' + message
+// }
+
+function trackToString(music) {
+  return music.album + ' - ' + music.title;
+}
