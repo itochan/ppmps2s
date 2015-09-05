6da2af24aaa49a680e2dcd6280b15572b9d06b93
Kazunori Jo <itochan315@gmail.com>
2015-09-06 07:10:07 +0900

    Delete meaningless comments

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,20 +5,11 @@ function saveOptions() {
   chrome.storage.sync.set({
     webhookUrl: webhookUrl
   }, function() {
-    // Update status to let user know options were saved.
-    // var status = document.getElementById('status');
-    // status.textContent = 'Options saved.';
-    // setTimeout(function() {
-    //   status.textContent = '';
-    // }, 750);
     window.close();
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restoreOptions() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     webhookUrl: ''
   }, function(items) {
